59825b72edc6fc4e8bb6e309e00f5dae3f15457a
Brian Heidrich <brian.heidrich@icloud.com>
2020-10-21 16:30:56 -0500

    Added dep for System.CommandLine.DragonFruit

--- FileSpyder/Program.cs
+++ FileSpyder/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace FileSpyder
+﻿using System;
+using System.CommandLine.DragonFruit;
+
+namespace FileSpyder
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string filePath)
         {
+            Console.WriteLine($"Value of filepath is {filePath}");
         }
     }
 }
